bc157b7e69237b9cd81971088023df87aa64265c
Jeffrey Wong <jwong886@uwo.ca>
2022-01-15 13:24:45 -0500

    finished population

--- PopulateMatrix.py
+++ PopulateMatrix.py
@@ -1,5 +1,6 @@
 from CarnivalObject import CarnivalObject
 from random import randint
+import json
 
 #PopulateMatrix Function will return a 10x10 matrix
 #populated with CarnivalObject Objects
@@ -12,10 +13,38 @@ def PopulateMatrix():
         for coloms in range(0,10):
             tempMatrix[row][coloms] = CarnivalObject("path")
     
-    #Placing Objects
-    print(randint(0,9))
-    print(randint(0,9))
-    objectsToPlace = []
+    #Reading objects from JSON File
+    with open("CarnivalSetup.json") as file:
+        data = json.load(file)
     
+    #Placing Objects
+    for attractionNum in len(data["Attractions"]):
+        data["Attractions"][attractionNum]
+        errorInsert = False
+        
+        while (errorInsert):
+            rowValue = randint(0,9)
+            colomValue = randint(0,9)
+            
+            objectHeight = data["Attractions"][attractionNum]["height"]
+            objectWidth = data["Attractions"][attractionNum]["width"]
+            
+            if((rowValue + objectHeight <= 9) and (colomValue + objectWidth <= 9)):
+                for h in range (0,objectHeight):
+                    for w in range (0,objectWidth):
+                        if ((tempMatrix[rowValue + h][colomValue + w].getObjectName() != "path")):
+                            errorInsert = True
+            else:
+                errorInsert = True
 
+            if (errorInsert == False):
+                for h in range (0, objectHeight):
+                    for w in range (0, objectWidth):
+                        tempMatrix[h][w].setObjectName(data["Attractions"][attractionNum]["name"])
+                        tempMatrix[h][w].getObjectColor(data["Attractions"][attractionNum]["color"])
+           
+    for i in range(0,9):
+        for j in range(0,9):
+            print(tempMatrix[i][j])
+    
     return tempMatrix
--- test.py
+++ test.py
@@ -1,2 +1,61 @@
+from CarnivalObject import CarnivalObject
 from random import randint
-print(randint(0,9))
+import json
+import numpy as np
+w, h = 10 ,10
+tempMatrix = [[0 for x in range(w)] for y in range(h)]
+
+# Making every block a path
+# This ensures our
+for row in range(0, 10):
+    for coloms in range(0, 10):
+        tempMatrix[row][coloms] = CarnivalObject("path", "#964B00")
+
+# Reading objects from JSON File
+with open("CarnivalSetup.json") as file:
+    data = json.load(file)
+
+# Placing Objects
+for attractionNum in range(0, len(data["Attractions"])):
+    print("\n attractionNum: {}".format(attractionNum))
+    print(data["Attractions"][attractionNum]["name"])
+    
+    canInsert = False
+    while (not canInsert):
+        print("run Loop")
+        rowValue = randint(0, 9)
+        colomValue = randint(0, 9)
+        print("rowValue: {}".format(rowValue))
+        print("colomValue: {}".format(colomValue))
+        
+        
+        objectHeight = data["Attractions"][attractionNum]["height"]
+        objectWidth = data["Attractions"][attractionNum]["width"]
+        
+        canInsert = True
+        if((rowValue + objectHeight <= 9) and (colomValue + objectWidth <= 9)):
+            for h in range(0, objectHeight):
+                for w in range(0, objectWidth):
+                    if ((tempMatrix[rowValue + h][colomValue + w].getObjectName() != "path")):
+                        errorInsert = False          
+        else:
+            canInsert = False
+
+        if (canInsert == True):
+            print("pack")
+            for h in range(rowValue, rowValue+objectHeight):
+                for w in range(colomValue, colomValue+objectWidth):
+                    tempMatrix[h][w].setObjectName(
+                        data["Attractions"][attractionNum]["name"])
+                    tempMatrix[h][w].setObjectColor(
+                        data["Attractions"][attractionNum]["color"])
+            
+
+for i in range(0, 10):
+    print("\n")
+    
+    line = ""
+    for j in range(0, 10):
+        # print(tempMatrix[i][j].getObjectName())
+        line = line + (tempMatrix[i][j].getObjectName()) + " "
+    print(line)   
